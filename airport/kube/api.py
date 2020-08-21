2b7872f797d34a1d615e4c11ca87689985e673d1
Xu zhengyu <zen-xu@outlook.com>
2020-08-21 15:03:15 +0800

    feat(kube:api): add volume models

--- airport/kube/api.py
+++ airport/kube/api.py
@@ -1,15 +1,20 @@
 from datetime import datetime
+from decimal import Decimal
+from enum import Enum
 from typing import Dict
 from typing import List
 from typing import Optional
 from uuid import UUID
 
 from pydantic import BaseModel
-from pydantic.fields import Field
 
 
 class KubeModel(BaseModel):
-    pass
+    ...
+
+
+class KubeEnum(str, Enum):
+    ...
 
 
 class TypeMeta(KubeModel):
@@ -43,3 +48,85 @@ class ObjectMeta(KubeModel):
     finalizers: List[str] = []
     clusterName: Optional[str]
     # managedFields not needed
+
+
+class PersistentVolumeAccessMode(KubeEnum):
+    ReadWriteOnce = "ReadWriteOnce"
+    ReadOnlyMany = "ReadOnlyMany"
+    ReadWriteMany = "ReadWriteMany"
+
+
+class PersistentVolumeMode(KubeEnum):
+    Block = "Block"
+    Filesystem = "Filesystem"
+
+
+class LabelSelectorOperator(KubeEnum):
+    In = "In"
+    NotIn = "NotIn"
+    Exists = "Exists"
+    DoesNotExist = "DoesNotExist"
+
+
+class LabelSelectorRequirement(KubeModel):
+    key: str
+    operator: LabelSelectorOperator
+    values: List[str] = []
+
+
+class LabelSelector(KubeModel):
+    matchLabels: Dict[str, str] = {}
+    matchExpressions: List[LabelSelectorRequirement] = []
+
+
+class ResourceName(KubeEnum):
+    CPU = "cpu"
+    Memory = "memory"
+    Storage = "storage"
+    EphemeralStorage = "ephemeral-storage"
+
+
+ResourceList = Dict[ResourceName, Decimal]
+
+
+class ResourceRequirements(KubeModel):
+    limits: Optional[ResourceList] = {}
+    requests: Optional[ResourceList] = {}
+
+
+class TypedLocalObjectReference(KubeModel):
+    kind: str
+    name: str
+    apiGroup: Optional[str]
+
+
+class PersistentVolumeClaimSpec(KubeModel):
+    accessModes: List[PersistentVolumeAccessMode] = []
+    selector: Optional[LabelSelector]
+    resources: Optional[ResourceRequirements]
+    storageClassName: Optional[str]
+    volumeMode: Optional[PersistentVolumeMode]
+    dataSource: Optional[TypedLocalObjectReference]
+
+
+class PersistentVolumeClaimPhase(KubeEnum):
+    Pending = "Pending"
+    Bound = "Bound"
+    Lost = "Lost"
+
+
+class PersistentVolumeClaimCondition(KubeEnum):
+    Resizing = "Resizing"
+    FileSystemResizePending = "FileSystemResizePending"
+
+
+class PersistentVolumeClaimStatus(KubeModel):
+    phase: Optional[PersistentVolumeClaimPhase]
+    accessModes: List[PersistentVolumeAccessMode] = []
+    capacity: ResourceList = {}
+    conditions: List[PersistentVolumeClaimCondition] = []
+
+
+class PersistentVolumeClaim(TypeMeta, ObjectMeta):
+    spec: Optional[PersistentVolumeClaimSpec]
+    status: Optional[PersistentVolumeClaimStatus]
