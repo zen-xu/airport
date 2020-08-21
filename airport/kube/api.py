e0ba364f723aa1408f220f0ea82b547ed3e5d93d
Xu zhengyu <zen-xu@outlook.com>
2020-08-21 11:23:55 +0800

    feat(kube:api): add `TypeMeta` and `ObjectMeta` models

--- airport/kube/api.py
+++ airport/kube/api.py
@@ -1,5 +1,45 @@
+from datetime import datetime
+from typing import Dict
+from typing import List
+from typing import Optional
+from uuid import UUID
+
 from pydantic import BaseModel
+from pydantic.fields import Field
 
 
 class KubeModel(BaseModel):
     pass
+
+
+class TypeMeta(KubeModel):
+    kind: Optional[str]
+    apiVersion: Optional[str]
+
+
+class OwnerReference(KubeModel):
+    apiVersion: str
+    kind: str
+    name: str
+    uid: Optional[UUID]
+    controller: Optional[bool]
+    blockOwnerDeletion: Optional[bool]
+
+
+class ObjectMeta(KubeModel):
+    name: Optional[str]
+    generateName: Optional[str]
+    namespace: Optional[str]
+    selfLink: Optional[str]
+    uid: Optional[UUID]
+    resourceVersion: Optional[str]
+    generation: Optional[int]
+    creationTimestamp: Optional[datetime]
+    deletionTimestamp: Optional[datetime]
+    deletionGracePeriodSeconds: Optional[int]
+    labels: Dict[str, str] = {}
+    annotations: Dict[str, str] = {}
+    ownerReferences: List[OwnerReference] = []
+    finalizers: List[str] = []
+    clusterName: Optional[str]
+    # managedFields not needed
