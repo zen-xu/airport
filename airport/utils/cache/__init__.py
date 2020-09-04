ce527158de5ce34df2f69f656cf9ad6289be04cd
ZhengYu, Xu <zen-xu@outlook.com>
2020-09-04 17:01:45 +0800

    feat: reexport HeapError

--- airport/utils/cache/__init__.py
+++ airport/utils/cache/__init__.py
@@ -1,5 +1,6 @@
 from .heap import Heap
 from .heap import HeapClosed
+from .heap import HeapError
 from .heap import HeapKeyFuncError
 from .heap import HeapLessFuncError
 from .heap import HeapObjectNotFound
