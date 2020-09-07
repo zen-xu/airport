6b2e9a67ce1eefaf39ce8b9439deafab57a877dc
ZhengYu, Xu <zen-xu@outlook.com>
2020-09-07 10:20:45 +0800

    feat: reexport `NamespaceInfo` and `NamespaceCollection`

--- airport/scheduler/api/__init__.py
+++ airport/scheduler/api/__init__.py
@@ -1,5 +1,7 @@
 from .enums import TaskStatus
 from .job_info import JobInfo
 from .job_info import TaskInfo
+from .namespace_info import NamespaceCollection
+from .namespace_info import NamespaceInfo
 from .pod_group_info import PodGroup
 from .resource_info import Resource
